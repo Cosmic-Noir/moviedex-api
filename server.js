cf7ea5977c07ce7ef18fe34aef79292df39322c2
Kristen Doucette <cosmicnoir90@gmail.com>
2019-09-23 21:02:19 -0600

    Correctly filter for genre

--- server.js
+++ server.js
@@ -2,6 +2,7 @@ const express = require("express");
 const morgan = require("morgan");
 const cors = require("cors");
 const helmet = require("helmet");
+const DATA = require("./movieData.json");
 
 const app = express();
 
@@ -10,7 +11,20 @@ app.use(cors());
 app.use(helmet());
 
 app.get("/movie", (req, res) => {
-  res.send("Yay it worked!");
+  let response = DATA;
+
+  // Validate correct data
+  if (req.query.genre) {
+    response = response.filter(movie =>
+      movie.genre.toLowerCase().includes(req.query.genre.toLowerCase())
+    );
+  }
+  if (req.query.country) {
+  }
+  if (req.query.avg_vote) {
+  }
+
+  res.json(response);
 });
 
 app.listen(8000, () => {
