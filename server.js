3038d14118552c322fa388d9801424f73f3786bb
Kristen Doucette <cosmicnoir90@gmail.com>
2019-09-23 20:54:40 -0600

    Add '/movie' path with success response from Postman

--- server.js
+++ server.js
@@ -1,13 +1,17 @@
 const express = require("express");
 const morgan = require("morgan");
 const cors = require("cors");
-const helmet = require("helment");
+const helmet = require("helmet");
 
 const app = express();
 
 app.use(morgan("morgan"));
 app.use(cors());
 app.use(helmet());
+
+app.get("/movie", (req, res) => {
+  res.send("Yay it worked!");
+});
 
 app.listen(8000, () => {
   console.log("Server started on PORT 8000");
