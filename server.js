2c5c7d2acaa94cf1749d6021fa5c803820fe1bba
Kristen Doucette <cosmicnoir90@gmail.com>
2019-09-24 19:01:59 -0600

    Add Procfile with startup script

--- server.js
+++ server.js
@@ -13,6 +13,16 @@ app.use(morgan(morganSetting));
 app.use(cors());
 app.use(helmet());
 
+app.use((error, req, res, next) => {
+  let response;
+  if (process.env.NODE_ENV === "production") {
+    response = { error: { message: "server error" } };
+  } else {
+    response = { error };
+  }
+  res.status(500).json(response);
+});
+
 // Validate bearerToken
 app.use(function validateToken(req, res, next) {
   const apiToken = process.env.API_TOKEN;
